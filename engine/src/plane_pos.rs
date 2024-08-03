c368613a0345299035545a7080e32b66fa1a56be
7d <61975820+iiiii7d@users.noreply.github.com>
2024-08-03 20:38:54 +0800

    altitude motion

--- engine/src/plane_pos.rs
+++ engine/src/plane_pos.rs
@@ -26,7 +26,7 @@ pub enum FlightPlan {
     Turn { angle: f32, radius: f32 },
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct Kinematics {
     target_sy: Option<f32>,
     target_vxz: Option<f32>,
@@ -38,5 +38,69 @@ pub struct Kinematics {
 impl PlanePos {
     pub fn tick(&mut self, dt: f32) {
         todo!()
+    }
+}
+
+impl Kinematics {
+    pub fn tick(&mut self, dt: f32, pos_ang: Pos3Angle, model_motion: ModelMotion) -> Vec2 {
+        // https://gamedev.stackexchange.com/questions/73627/move-a-2d-point-to-a-target-first-accelerate-towards-the-target-then-decelerat
+        if let Some(target_sy) = self.target_sy {
+            // if (target_sy - pos_ang.0.y).abs() <= (self.v.y * dt).abs() {
+            //     ds_override.y = target_sy - pos_ang.0.y;
+            //     self.v.y = 0.0;
+            //     self.target_sy = None;
+            //     self.a.y = 0.0;
+            // } else
+            if self.v.y.powi(2) / (2.0 * model_motion.ver_accel) <= target_sy - pos_ang.0.y {
+                self.a.y = model_motion.ver_accel.copysign(target_sy - pos_ang.0.y);
+            } else {
+                self.a.y = model_motion.ver_accel.copysign(pos_ang.0.y - target_sy);
+            }
+        }
+
+        self.v += self.a * dt;
+        self.v.y = self
+            .v
+            .y
+            .clamp(-model_motion.max_ver_vel, model_motion.max_ver_vel);
+        let mut ds = self.v * dt;
+
+        if let Some(target_sy) = self.target_sy {
+            if (target_sy - pos_ang.0.y).abs() <= (self.v.y * dt).abs() {
+                ds.y = target_sy - pos_ang.0.y;
+                self.target_sy = None;
+                self.a.y = 0.0;
+            }
+        }
+
+        ds
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::util::{angle::Angle, Pos3};
+
+    #[test]
+    fn ascend() {
+        let mut k = Kinematics::default();
+        k.target_sy = Some(123.0);
+        let mut pos_ang = Pos3Angle(Pos3::ZERO, Angle(0.0));
+        let model_motion = ModelMotion {
+            max_hor_vel: 0.0,
+            hor_accel: 0.0,
+            max_ver_vel: 30.0,
+            ver_accel: 5.0,
+            turning_radius: 0.0,
+        };
+        for _ in 0..100 {
+            pos_ang.0.y += k.tick(1.0, pos_ang, model_motion).y;
+            eprintln!("{:?}", pos_ang.0.y);
+            if k.target_sy.is_none() {
+                break;
+            }
+        }
+        assert_eq!(k.target_sy, None);
     }
 }
