cbe57d9742ebc7fe16d435aa0d2cc56c6e6bff37
7d <61975820+iiiii7d@users.noreply.github.com>
2025-01-06 03:10:06 +0800

    fix tooltip zoom bug

--- client/src/map.ts
+++ client/src/map.ts
@@ -3,7 +3,25 @@ import config from "./config";
 
 export const map = ref<L.Map>();
 
+// @ts-ignore
+// https://stackoverflow.com/questions/73542576/leaflet-error-when-zooming-after-closing-popup
+function leafletZoomFix() {
+  L.Tooltip.prototype._animateZoom = function (opt) {
+    if (!this._map) {
+      return;
+    }
+    const pos = this._map._latLngToNewLayerPoint(
+      this._latlng,
+      opt.zoom,
+      opt.center,
+    );
+    this._setPosition(s);
+  };
+}
+
 export function initMap() {
+  leafletZoomFix();
+
   map.value = L.map("map", {
     preferCanvas: true,
     crs: L.CRS.Simple,
